d7b97f8ba8ab1870b55676f11d0add9fb3fd8813
Aslam <mdaslamsikder83@gmail.com>
2023-08-06 10:04:40 +0600

    first deploy vercel

--- next.config.js
+++ next.config.js
@@ -1,19 +1,13 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 module.exports = {
   experimental: {
     appDir: true,
   },
   images: {
-    domains: ['s3.amazonaws.com', '127.0.0.1']
+    domains: ['s3.amazonaws.com', 'aslam84.pythonanywhere.com']
   }
 
 }
 
 
 
-=======
-const nextConfig = {}
-
-module.exports = nextConfig
->>>>>>> 787151931feffaf81d3f5a66dbb0029e49dd4f0b
